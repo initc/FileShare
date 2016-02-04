c90b46e4ccf119082d90c2e8fd20044f2d331347
initc <1875919175@qq.com>
2016-02-04 18:39:43 +0800

    android project

---
+++ src/com/jie/view/MainActivity.java
@@ -0,0 +1,20 @@
+package com.jie.view;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.jie.fileshare.R;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.filemanagerview);
+		
+	}
+	
+	
+	
+}
